fda147dca22ad52ea0f4f7211dc7388a5f023302
EdyTola <jaircitola@gmail.com>
2024-09-12 21:44:33 -0500

    Add files via upload

---
+++ CalcFx/src/main/java/pe/edu/upeu/calcfx/servicio/CalcServiceI.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.calcfx.servicio;
+
+import pe.edu.upeu.calcfx.modelo.CalcTO;
+
+import java.util.List;
+
+public interface CalcServiceI {
+
+    public void guardarResultados(CalcTO to);//C
+    public List<CalcTO> obtenerResultados();//R
+    public void eliminarResultados(int index);//U
+    public void actulizarResultados (CalcTO to, int index);//D
+}
